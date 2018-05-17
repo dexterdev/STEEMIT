d65fc93f6269d455e4a0bc5c854139c9a7c57806
Devanand T <devanand.t.1986@gmail.com>
2018-05-17 15:02:22 +0530

    Update diffusion_.m

--- DIFFUSION_from_Random_Walk/diffusion_.m
+++ DIFFUSION_from_Random_Walk/diffusion_.m
@@ -11,10 +11,6 @@ y=101;
 space_2d(x,y,1)=1;
 % We are going to play random walk here
 
-direction=randi(4);
-% randi(4) is a uniform distributed integer which spits out integers 1 to 4
-% Let 1 imply right,2 - up, 3 - left, 4 - down
-
 for repeat=1:1000
 x=101;
 y=101;
